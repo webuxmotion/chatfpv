50b713615af78a6a55e9133c25857c2f35ffe30c
webuxmotion <pereverziev.andrii@gmail.com>
2024-07-01 19:27:21 +0300

    Profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Profile from "@components/profile"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react"
+
+const MyProfile = () => {
+    const { data: session } = useSession();
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+    
+          setPosts(data);
+        }
+    
+        if (session?.user.id) {
+            fetchPosts();
+        }
+        
+      }, [session?.user.id])
+
+    const handleEdit = () => {
+        
+    }
+
+    const handleDelete = async () => {
+
+    }
+
+  return (
+    <Profile
+        name="My"
+        desc="Welcome to your personalized profile page"
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,10 +1,16 @@
 "use client"
 
+import { useSession } from "next-auth/react";
 import Image from "next/image"
+import { usePathname } from "next/navigation";
+import { useRouter } from 'next/navigation'
 import { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState(false);
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -51,6 +57,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
