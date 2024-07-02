930218e2f4e0094134e418e1b9ae65fda9671bbd
webuxmotion <pereverziev.andrii@gmail.com>
2024-07-03 02:17:07 +0300

    Fix import typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Profile from "@components/profile"
+import Profile from "@components/Profile"
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
